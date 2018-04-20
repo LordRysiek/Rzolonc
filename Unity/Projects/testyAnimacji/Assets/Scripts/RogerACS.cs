c4919ef1f0eaf38765a48589a6a53a8a9fbcceda
DESKTOP-RJHKG6H\KubaBilski <jakub.bilski@autograf.pl>
2018-04-20 02:53:11 +0200

    Po dlugiej walce zaimportowalem model do Unity. \n \n Przelaczanie pomiedzy poszczegolnymi animacjami przebiega poprawnie \n Brakuje animacji: RogerRunLeftPivot i RogerRunRight (nie zrobilem ich po prostu)\n Przelaczanie sie bezposrednio pomiedzy animacjami, bez blendowania, to katastrofa. \n Powoduje to opoznienie w reakcji bohatera na wcisniete klawisze, a to jest dosyc mocno niedopuszczalne. \n Zeby blendowac trzeba chyba stworzyc inne animacje, musze o tym poczytac. \n Ogolnie animator jest calkiem fajnym narzedziem. \n Zastanawiam sie, czy nie sprobowac przelaczania sie animacji na rzadanie, a nie po ostatniej klatce. \n No i oczywiscie pozostaje kwestia obracania sie modelu (tu sie nie obraca). \n Mozna to zrobic rigiem Unity albo na chama skryptami do kazdej animacji. \n Ogolnie ide spac :3

---
+++ Unity/Projects/testyAnimacji/Assets/Scripts/RogerACS.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RogerACS : MonoBehaviour {
+
+
+    private Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+        UpdateDirectionAndIsRunning();
+    }
+
+    void UpdateDirectionAndIsRunning()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        string directionName = "DirectionClockwise";
+        anim.SetBool("IsRunning", horizontal != 0 || vertical != 0);
+        if (horizontal == 0)
+        {
+            if (vertical > 0)     //prosto
+            {
+                anim.SetInteger(directionName, 0);
+            }
+            else if (vertical == 0)     //stoi
+            {
+                anim.SetInteger(directionName, 0);
+            }
+            else if (vertical < 0)     //do tylu
+            {
+                anim.SetInteger(directionName, 4);
+            }
+        }
+        else if (horizontal > 0)
+        {
+            if (vertical > 0)     //w prawo i prosto
+            {
+                anim.SetInteger(directionName, 1);
+            }
+            else if (vertical == 0)     //w prawo
+            {
+                anim.SetInteger(directionName, 2);
+            }
+            else if (vertical < 0)     //w prawo i do tylu
+            {
+                anim.SetInteger(directionName, 3);
+            }
+        }
+        else if (horizontal < 0)
+        {
+            if (vertical > 0)     //w lewo i prosto
+            {
+                anim.SetInteger(directionName, 7);
+            }
+            else if (vertical == 0)     //w lewo
+            {
+                anim.SetInteger(directionName, 6);
+            }
+            else if (vertical < 0)     //w lewo i do tylu
+            {
+                anim.SetInteger(directionName, 5);
+            }
+        }
+    }
+}
---
+++ Unity/Projects/testyAnimacji/Assets/Scripts/RotateAnimation45.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAnimation45 : StateMachineBehaviour
+    {
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.gameObject.transform.Rotate(0, 45, 0);
+    }
+}
